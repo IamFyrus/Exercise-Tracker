65c9dbd35f682d7ac4bdbc6e4aa6ae5230337d56
IamFyrus <cyrusazad2442@gmail.com>
2025-05-07 13:29:36 -0700

    a

--- index.js
+++ index.js
@@ -13,8 +13,13 @@ app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/index.html')
 });
 
-mongoose.connect('mongodb+srv://cyrusazad2442:678Triple98212%21@iamfyrus.ipjv5kz.mongodb.net/?retryWrites=true&w=majority&appName=IamFyrus', { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+}).then(() => console.log('MongoDB connected'))
+  .catch(err => console.error('MongoDB connection error:', err))
 
+  
 const userSchema = new mongoose.Schema({
   username: { type: String, required: true }
 })
