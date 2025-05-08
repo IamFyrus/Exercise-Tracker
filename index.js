cfe3c769355348c312f2b3b76a33601ab4069aae
IamFyrus <cyrusazad2442@gmail.com>
2025-05-08 16:22:41 -0700

    Update index.js

--- index.js
+++ index.js
@@ -13,39 +13,31 @@ app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/index.html')
 });
 
-mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-}).then(() => console.log('MongoDB connected'))
-  .catch(err => console.error('MongoDB connection error:', err))
+mongoose.connect(process.env.MONGO_URI)
 
-  
+
 const userSchema = new mongoose.Schema({
   username: { type: String, required: true }
 })
 
 const exerciseSchema = new mongoose.Schema({
-  userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+  user_id: { type: String, required: true},
   description: String,
   duration: Number,
-  date: Date
+  date: Date,
 })
 
 const User = mongoose.model('User', userSchema)
 const Exercise = mongoose.model('Exercise', exerciseSchema)
 
-app.post('/api/users', async (req, res) => {
-  try {
-    const user = new User({ username: req.body.username })  
-    const savedUser = await user.save()
-    res.json({ username: savedUser.username, _id: savedUser._id })
-  } catch (err) {
-    res.status(500).json({ error: 'Error saving user' })
-  }
+
+app.post('/api/users', (req, res) => {
+  console.log(req.body)
+  res.json({hey: 'hey'})
 })
 
-app.get('/api/users', async (req, res) => {
-  const users = await User.find({}, 'username _id')
+/*app.get('/api/users', (req, res) => {
+  const users =  User.find({}, 'username _id')
   res.json(users)
 })
 
@@ -118,7 +110,7 @@ app.get('/api/users/:_id/logs', async (req, res) => {
   }
 })
 
-
+*/
 const listener = app.listen(process.env.PORT || 3000, () => {
   console.log('Your app is listening on port ' + listener.address().port)
 })
